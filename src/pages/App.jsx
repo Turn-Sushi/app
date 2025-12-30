16a579dd68726e210818876a75cf029dc0c66e96
frydaynar <nara1743@gmail.com>
2025-12-30 15:20:49 +0900

    최종

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,9 +1,19 @@
-import '@styles/App.css'
+import '@styles/App.css';
+import Page1 from './page1.jsx';
+import Page2 from './page2.jsx';
+import Page3 from './page3.jsx';
+import Page4 from './page4.jsx';
+import Page5 from './page5.jsx';
+
 function App() {
   return (
-    <>
-      <Frankie />
-    </>
+    <ul>
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Page4 />
+      <Page5 />
+    </ul>
   )
 }
 
